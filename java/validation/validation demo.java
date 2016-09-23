eceda07b6f7b30cd9ca279e0eba2df7cafb6db71
lizebin0918 <lizebin0918@163.com>
2016-09-23 16:53:43 +0800

    添加方法参数校验的demo,满足简单需求

---
+++ java/validation/validation demo.java
@@ -0,0 +1,33 @@
+/*简单的方法参数校验*/
+
+/*0.包导入*/
+import javax.validation.constraints.*;
+import org.springframework.validation.beanvalidation.*;
+
+/*1.声明需校验参数的 Bean, 并注入到 spring 容器*/
+@Validated
+public class TestService {
+
+    public void validationId(@Min(value = 4) Integer id, @NotNull String name, @NotEmpty String pwd) {
+        System.out.println(name);
+        System.out.println(id.intValue());
+        System.out.println(pwd);
+    }
+}
+
+/*2.声明后置校验处理器*/
+@Bean
+public MethodValidationPostProcessor getValidationPostProcessor() {
+    return new MethodValidationPostProcessor();
+}
+
+/*3.调用者捕获 ConstraintViolationException 异常，说明参数有误*/
+@Test
+public void test12() {
+    try {
+        testService.validationId(new Integer(3), "123", new String());
+    } catch (ConstraintViolationException e) {
+        log.error(e.getMessage(), e);
+    }
+}
+
