ec78a8eea6688096c21c25848f8160a9faabbf39
陈皮 <chenpi@sinoxk.com>
2017-03-16 16:52:22 +0800

    频次统计帮助类

--- java/频次计算器/FrequencyHelper.java
+++ java/频次计算器/FrequencyHelper.java
@@ -1,3 +1,18 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Random;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * 频次帮助类(用于限制调用频次)
+ * timeUnit=TimeUnit.MINUTE,interval=1,limit=1000:表示一分钟最多只能执行1000次；如果超过上限，会返回false，只有等下一个周期，才会重设
+ * <br/>
+ * Created on : 2017-03-15 23:21
+ * @author lizebin
+ */
 public class FrequencyHelper {
 
     /**
@@ -23,16 +38,15 @@ public class FrequencyHelper {
     /**
      * 计数器
      */
-    private ConcurrentHashMap<String, LongAdder> counter = new ConcurrentHashMap<>();
+    private ConcurrentHashMap<String, AtomicLong> counter = new ConcurrentHashMap<>();
 
     private FrequencyHelper() {}
 
     /**
      * 构造函数
-     * timeUnit=TimeUnit.MINUTE,interval=1,limit=1000:表示一分钟最多只能执行1000次
      * @param timeUnit 时间间隔单位
      * @param interval 时间间隔数值
-     * @param max 最大次数
+     * @param limit 最大次数
      * @return
      */
     public static FrequencyHelper newInstance(ChronoUnit unit, int interval, long limit) {
@@ -48,20 +62,30 @@ public class FrequencyHelper {
      * @return
      */
     public boolean canCall(String key) {
-        LocalDateTime now = LocalDateTime.now();
-        LongAdder count = counter.computeIfAbsent(key, (k) -> {
-            return new LongAdder();
+        AtomicLong count = counter.computeIfAbsent(key, (k) -> {
+            return new AtomicLong();
         });
-        //上次调用时间 - 当前时间 < 时间间隔
-        if(interval > TimeUtils.getOffset(timeUnit, lastCallDatetime, now)) {
-            count.increment();
-        } else {
-            lastCallDatetime = LocalDateTime.now();
-            count.reset();
+        //上次调用时间 - 当前时间 >= 时间间隔:需要重设计数器
+        if(interval <= timeUnit.between(lastCallDatetime, LocalDateTime.now())) {
+            synchronized (this) {
+                if(interval <= timeUnit.between(lastCallDatetime, LocalDateTime.now())) {
+                    lastCallDatetime = LocalDateTime.now();
+                    count.set(0L);
+                }
+            }
         }
-        if(count.longValue() > limit) {
-            return false;
+        if(count.incrementAndGet() <= limit) {
+            return true;
         }
-        return true;
+        return false;
     }
+
+    /*public static void main(String[] args) throws InterruptedException {
+        FrequencyHelper instance = FrequencyHelper.newInstance(ChronoUnit.SECONDS, 1, 1);
+        if(instance.canCall("a")) {//可执行
+
+        } else {//不可执行
+
+        }
+    }*/
 }
